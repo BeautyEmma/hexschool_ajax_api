2effb9e1af66d7dac8131d866fa0b7ea29ce3896
ItsEms <emma801006@hotmail.com>
2017-03-23 23:45:05 +0800

    yeeeeeeeeeees
    finaaaaaaallyyyyyy

--- js/all.js
+++ js/all.js
@@ -1,33 +1,138 @@
 $(function () {
-    var test = 'Test if Append works';
-    $('.wrap').append(test);
-    $.ajax({
-        type: 'GET',
-        url: 'data.json',
-        //async: true,
-        dataType: 'json',
-        success: function (data) {
 
-            console.log('Load data Success, and the Type of data is : ' + typeof (data));
+    $('.search').on('click', function (e) {
 
-            for (i = 0; data.length > i; i++) {
+        //creat map
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 13,
+            center: {
+                lat: 22.625524,
+                lng: 120.29801700000007
+            }
+        });
 
-                var thisData = data;
-                var str;
-                var dis = thisData[i].sarea;
+        //define geocoder
+        var geocoder = new google.maps.Geocoder();
 
-                if (dis == '信義區') {
-                    str = '<li>' + thisData[i].iid + '</li>';
-                    $('body').append(str);
-                }
-            } //end of for
-            console.log('End of for');
-        } //end of success
-    });
+        e.preventDefault();
+        $.ajax({
+            type: 'GET',
+            async: true,
+            dataType: 'json',
+            url: 'http://opendata.khcc.gov.tw/public/OD_ksml_info.ashx',
+            success: function (data) {
 
-});
+                var dis = $('#dis').val();
+
+                //var thisData = JSON.parse(data);
+                var thisData = jQuery.parseJSON(JSON.stringify(data));
+
+                $('.result').html('');
+
+                for (i = 0; thisData.length > i; i++) {
+
+                    name = thisData[i].Title;
+
+                    if (thisData[i].Address.indexOf(dis) > -1) {
+                        str = '<li>' + thisData[i].Title + '</li>' + '<p>地址 : ' + thisData[i].Address + '</p>';
+                        //addr.push(thisData[i].Address);
+                        $('.result').append(str);
+
+                        geocodeAddress(geocoder, map);
+
+                        function geocodeAddress(geocoder, resultsMap) {
+
+                            //定義address等於str裡的address
+                            //var address = document.getElementById('address').value;
+                            //var address = document.getElementsByClassName('.address')[0].value ;
+                            //var addr = ['高雄市前鎮區新光路61號', '高雄市前鎮區新光路61號'];
+                            var address = thisData[i].Address;
+
+                            //引數帶進來的geocoder的geocode
+                            geocoder.geocode({
+                                'address': address
+                            }, function (results, status) {
+                                if (status === google.maps.GeocoderStatus.OK) {
+                                    
+                                    resultsMap.setCenter(results[0].geometry.location);
+                                    console.log(results[0].geometry.location);
+                                    
+                                    var marker = new google.maps.Marker({
+                                        map: resultsMap,
+                                        position: results[0].geometry.location
+                                    });
+                                    console.log('location: '+results[0].geometry.location)
+                                } else {
+                                    alert('Geocode was not successful for the following reason: ' + status);
+                                } // end of else
+                            }); // end of geocoder.geocode
+                        } // end of geocodeAddress function
+                    } // end of if
+                } // end of for
+            } // end of success function
+        }); // end of ajax get data
+    }); //end of dis click function
+}); // end of page load function
+
+
+//
+//function initMap() {
+//    var map = new google.maps.Map(document.getElementById('map'), {
+//        zoom: 13,
+//        center: {
+//            lat: 22.625524,
+//            lng: 120.29801700000007
+//        }
+//    });
+//
+//    //開始使用，定義geocoder
+//    var geocoder = new google.maps.Geocoder();
+//
+//    //如果輸入按鈕click了就執行function裡的geocodeAddress帶入參數geocoder,map
+//    document.getElementById('submit').addEventListener('click', function () {
+//        geocodeAddress(geocoder, map);
+//    });
+//
+//}
+//
+//function geocodeAddress(geocoder, resultsMap) {
+//
+//    //定義address等於str裡的address
+//    //var address = document.getElementById('address').value;
+//    //var address = document.getElementsByClassName('.address')[0].value ;
+//    var addr = '高雄市前鎮區新光路61號';
+//    var address = addr;
+//    console.log('var address: ' + address);
+//
+//    //引數帶進來的geocoder的geocode
+//    geocoder.geocode({
+//        'address': address
+//        //address屬性為上面定義的address(輸入框address的值)
+//
+//    }, function (results, status) {
+//        if (status === google.maps.GeocoderStatus.OK) {
+//
+//            //如果status是ok的，引數resultsMap帶入第二層function引數result陣列的第一個資料的位置
+//            resultsMap.setCenter(results[0].geometry.location);
+//
+//            var marker = new google.maps.Marker({
+//
+//                //新增一個marker，把上面的位置丟進來
+//                map: resultsMap,
+//                position: results[0].geometry.location
+//
+//            });
+//        } else {
+//            alert('Geocode was not successful for the following reason: ' + status);
+//        }
+//    });
+//}
 
 /*
+
+//TODO 現在問題是 在上面的function有用了addr陣列儲存了區域選擇清單後的地址，但是全域問題，沒辦法把這個addr陣列裡的地址放進googlemap裡面，又沒辦法不使用initMap，需要嘗試把新增marker的function放進for迴圈裡面
+
+//TODO嘗試是否不需要用callback initMap 這樣就可以把function都丟在一起
 
 var thisData = data;
                 //var thisData = JSON.parse(data);
